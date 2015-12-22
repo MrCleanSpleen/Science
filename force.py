fa4926ab723612903b59dde82b366fbe2a087122
Alexander Green <aeg0322@gmail.com>
2015-12-22 18:35:30 -0500

    save

--- force.py
+++ force.py
@@ -2,11 +2,15 @@ import skilstak.colors as c
 import math
 
 def force():
-    print(c.cl + "What is the mass of the object in meters?")
-    m = input(">>> ")
+    """Uses Newton's second law to find the force acting on an object.
+
+    F = m x a
+    """
+    print(c.cl + "What is the mass of the object in kilograms?")
+    m = int(input(">>> "))
     print(c.cl + "What is the acceleration of the object, in m/s^2?")
-    a = input(">>> ")
-    m = int()
-    a = int()
-    f = a * m
-    print(c.cl + "The force acting on the object is ",f + "joules.")
+    a = int(input(">>> "))
+    m1 = float(m)
+    a1 = float(a)
+    f = float(a1 * m1)
+    print(c.cl + "The force acting on the object is " + str(f) + " joules.")
