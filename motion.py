538c7b51aeacbf17e06d12bf672926d2960a3005
Alexander Green <aeg0322@gmail.com>
2015-12-22 19:02:55 -0500

    save

---
+++ motion.py
@@ -0,0 +1,7 @@
+import skilstak.colors as c
+import math
+import time
+
+def solve_kinetic():
+    m = int(input(c.cl + "What is the mass of your object? >>> "))
+    v = int(input(c.cl + "What is the velocity, in m/s, of your object? >>> "))
